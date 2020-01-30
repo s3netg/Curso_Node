4053867325e688436f6fc3a0783639790b03847d
sergio <sergio-dos-santos-silva@hotmail.com>
2020-01-30 16:05:14 -0300

    node

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+app.listen(3000,function(){
+    console.log("Escutando na porta 3000")
+});
+app.get('/', function(req, resp) {
+    resp.send(
+        `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Casa do Código </h1>
+                </body> 
+            </html>
+        `
+    );
+});
+app.get('/livros', function(req, resp) {
+    resp.send(
+        `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Listagem de livros </h1>
+                </body> 
+            </html>
+        `
+    );
+});
